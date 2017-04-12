0caf44174dcd516d31e8e53868573913287cfdf5
Luke Cavanagh <lukecav@users.noreply.github.com>
2017-04-12 12:28:12 -0600

    Update plugincore.php

--- plugincore.php
+++ plugincore.php
@@ -1,17 +1,17 @@
 <?php
 /*
   Plugin Name: My Shortcodes
-  Plugin URI: http://myshortcodes.cramer.co.za
+  Plugin URI: https://github.com/lukecav/My-Shortcodes
   Description: Build advanced custom plugins, widgets shortcodes and web elements to use within your site or distribute to others as standalone plugins or included in your themes.
-  Author: David Cramer
-  Version: 2.04
-  Author URI: http://digilab.co.za
+  Author: Luke Cavanagh
+  Version: 2.05
+  Author URI: https://github.com/lukecav
  */
 
 //initilize plugin
 define('MYSHORTCODES_PATH', plugin_dir_path(__FILE__));
 define('MYSHORTCODES_URL', plugin_dir_url(__FILE__));
-define('MYSHORTCODES_VER', '2.04');
+define('MYSHORTCODES_VER', '2.05');
 
 require_once MYSHORTCODES_PATH . 'libs/functions.php';
 require_once MYSHORTCODES_PATH . 'libs/actions.php';
